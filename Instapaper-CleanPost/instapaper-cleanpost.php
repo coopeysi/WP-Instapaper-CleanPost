7625299ba24f79b2b55b448ac0a77418d0b0e8cc
Simon Coopey <mail@simoncoopey.net>
2012-02-20 15:39:44 +0000

    Slight typo in comments

--- Instapaper-CleanPost/instapaper-cleanpost.php
+++ Instapaper-CleanPost/instapaper-cleanpost.php
@@ -3,7 +3,7 @@
 Plugin Name: Instapaper CleanPost
 Plugin URI: http://www.simoncoopey.net/wordpress/plugins/instapaper-formatter/
 Description: Formats blog posts so that they appear correctly in Instapaper
-Version: 0.3.0
+Version: 0.3.1
 Author: Simon Coopey
 Author URI: http://www.simoncoopey.com/
 License: GPL3.0 | http://www.gnu.org/licenses/gpl-3.0.html
@@ -11,7 +11,7 @@ License: GPL3.0 | http://www.gnu.org/licenses/gpl-3.0.html
 
 //instapaper_title function from v0.1 removed. It should take $wp_title as title by default
 
-//Adding an extra <div> with a class seems to work more consistently. v2.0 will use this function instead.
+//Adding an extra <div> with a class seems to work more consistently. v0.2 onwards will use this function instead.
 function instapaper_ce($content = ''){
   if(is_single()){ //Inserts the class "instapaper_body" around content - works on single post pages
     $content = '<div class="instapaper_body">' . "\n" . $content;
